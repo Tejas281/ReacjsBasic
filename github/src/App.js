629008301b169ce1a762400f19b2f800d9b87eef
Tejas281 <tejas1188245@gmail.com>
2021-09-13 13:43:20 +0530

    Local To store a Database In token

--- github/src/App.js
+++ github/src/App.js
@@ -1,30 +1,29 @@
-import React,{useState} from "react";
-import RegisterPage from './Component/RegisterPage'
-import LoginPage from "./Component/LoginPage"
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-
-} from "react-router-dom";
+import React, { useState } from 'react';
+import RegisterPage from './Component/RegisterPage';
+import LoginPage from './Component/LoginPage';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import Dashboard from './Component/Dashboard';
 
+// function getToken() {
+//   const tokenString = sessionStorage.getItem('token');
+//   const userToken = JSON.parse(tokenString);
+//   return userToken?.token;
+// }
 function App() {
-  
-const [token, setToken] = useState();
-if (!token) {
-  return <LoginPage setToken={setToken} />
-}
+  // const { token, setToken } = useToken();
+  // if (!token) {
+  //   return <LoginPage setToken={setToken} />;
+  // }
   return (
     <Router>
       <div>
         <Switch>
           <Route>
-          <Route  path="/Register" component={RegisterPage} exact/>
-          <Route path="/Dashboard" component={Dashboard} exact/>
+            <Route path='/Register' component={RegisterPage} exact />
+            <Route path='/Dashboard' component={Dashboard} exact />
+            <Route path='/LoginPage' component={LoginPage} exact />{' '}
           </Route>
-                </Switch>
-    
+        </Switch>
       </div>
     </Router>
   );
--- github/src/Component/LoginPage.js
+++ github/src/Component/LoginPage.js
@@ -1,5 +1,4 @@
-
-import React,{useState,useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
@@ -14,13 +13,14 @@ import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import axios from 'axios';
-import PropTypes  from 'prop-types';
+import PropTypes from 'prop-types';
+//import { Link } from 'react-router';
 
 function Copyright() {
   return (
-    <Typography variant="body2" color="textSecondary" align="center">
+    <Typography variant='body2' color='textSecondary' align='center'>
       {'Copyright © '}
-      <Link color="inherit" href="https://material-ui.com/">
+      <Link color='inherit' href='https://material-ui.com/'>
         Your Website
       </Link>{' '}
       {new Date().getFullYear()}
@@ -49,91 +49,100 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-
-
 export default function LoginPage() {
-    const classes = useStyles();
-    const api = ('http://localhost:5000/api/auth')
-  //  const token = (sessionStorage.getItem('token'));
-    const[user,setUser] = useState({})
-   console.log("user",user)
-    const { email,password } = user
-    const handleChange = (e) => {
-        setUser({ ...user, [e.target.name]: e.target.value })
-    }
-    const handleSubmit = (e) => {
-      e.preventDefault();
-     
-      axios.post(api, { ...user })
-        .then((res) => {
-          alert('User Login', res.data.user)   
-         
-        })
-            .catch((err) => {
-                alert('User Not EmailId-Password Not Correct ',err);
-                console.log(err)
-            })
-      
-    }
+  const classes = useStyles();
+  // const token = sessionStorage.setItem('token', JSON.stringify(token));
+  const api = 'http://localhost:5000/api/auth';
+  const [user, setUser] = useState({});
+  console.log('user', user);
+  const { email, password } = user;
+  const handleChange = (e) => {
+    setUser({ ...user, [e.target.name]: e.target.value });
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Form validation
+    // Form submit
+    // get response form server
+    // validate response
+    // If response is ok then set token to the localstorage and redirect to the dashboard, else show an error message.
+    // const token = localStorage.setItem('token', JSON.stringify(token));
+
+    axios
+      .post(api, { ...user })
+      .then((res) => {
+        const { token } = res.data;
+        localStorage.setItem('token', token);
+        localStorage.getItem('token');
+
+        alert('User Login', res.data.user);
+      })
+      .catch((err) => {
+        alert('User Not EmailId-Password Not Correct ', err);
+        console.log(err);
+      });
+    //localStorage.getItem(token);
+    //console.log('Token IS', token);
+  };
 
   return (
-    <Container component="main" maxWidth="xs">
+    <Container component='main' maxWidth='xs'>
       <CssBaseline />
       <div className={classes.paper}>
         <Avatar className={classes.avatar}>
           <LockOutlinedIcon />
         </Avatar>
-        <Typography component="h1" variant="h5">
+        <Typography component='h1' variant='h5'>
           Sign in
         </Typography>
         <form className={classes.form} onSubmit={handleSubmit} noValidate>
           <TextField
-            variant="outlined"
-            margin="normal"
+            variant='outlined'
+            margin='normal'
             required
             fullWidth
-            id="email"
-            label="Email Address"
-            name="email"
+            id='email'
+            label='Email Address'
+            name='email'
             value={email}
-            onChange={handleChange}          
-            autoComplete="email"
+            onChange={handleChange}
+            autoComplete='email'
             autoFocus
           />
           <TextField
-            variant="outlined"
-            margin="normal"
+            variant='outlined'
+            margin='normal'
             required
             fullWidth
-            name="password"
-              value={password}
-             onChange={handleChange}             
-            label="Password"
-            type="password"
-            id="password"
-            autoComplete="current-password"
+            name='password'
+            value={password}
+            onChange={handleChange}
+            label='Password'
+            type='password'
+            id='password'
+            autoComplete='current-password'
           />
           <FormControlLabel
-            control={<Checkbox value="remember" color="primary" />}
-            label="Remember me"
+            control={<Checkbox value='remember' color='primary' />}
+            label='Remember me'
           />
           <Button
-            type="submit"
+            type='submit'
             fullWidth
-            variant="contained"
-            color="primary"
+            variant='contained'
+            color='primary'
             className={classes.submit}
           >
             Sign In
           </Button>
           <Grid container>
             <Grid item xs>
-              <Link href="#" variant="body2">
+              <Link href='#' variant='body2'>
                 Forgot password?
               </Link>
             </Grid>
             <Grid item>
-              <Link href="#" variant="body2">
+              <Link href='#' variant='body2'>
                 {"Don't have an account? Sign Up"}
               </Link>
             </Grid>
@@ -147,5 +156,5 @@ export default function LoginPage() {
   );
 }
 LoginPage.prototypes = {
-  setToken :PropTypes.func.isRequired
-}
+  setToken: PropTypes.func.isRequired,
+};
