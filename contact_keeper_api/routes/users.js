dcc60de1a0f92985813880e3b4f1e375c9db7aba
Tejas281 <tejas1188245@gmail.com>
2021-09-14 16:02:33 +0530

    Get all User API call

--- contact_keeper_api/routes/users.js
+++ contact_keeper_api/routes/users.js
@@ -1,4 +1,4 @@
-require('dotenv').config()
+require('dotenv').config();
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
@@ -12,64 +12,86 @@ const User = require('../models/User');
 // @desc     Register user
 // @access   Public
 router.post(
-	'/',
-	[
-		check('firstName', 'Name is required')
-			.not()
-			.isEmpty(),
-		check('email', 'Please include a valid email').isEmail(),
-	check(
-			'password',
-			'Please enter a password with 6 or more characters'
-		).isLength({ min: 6 })
-	],
-	async (req, res) => {
-		const errors = validationResult(req);
-		console.log(req.body)
-		if (!errors.isEmpty()) {
-			console.log(errors.array())
-			return res.status(400).json({ errors: errors.array() });
-		}
+  '/',
+  [
+    check('firstName', 'Name is required').not().isEmpty(),
+    check('email', 'Please include a valid email').isEmail(),
+    check(
+      'password',
+      'Please enter a password with 6 or more characters'
+    ).isLength({ min: 6 }),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    console.log(req.body);
+    if (!errors.isEmpty()) {
+      console.log(errors.array());
+      return res.status(400).json({ errors: errors.array() });
+    }
 
-		const {firstName,lastName,email,phone,date,gender,password,confirm_password } = req.body;
+    const {
+      firstName,
+      lastName,
+      email,
+      phone,
+      date,
+      gender,
+      password,
+      confirm_password,
+    } = req.body;
 
-		try {
-			let user = await User.findOne({ email });
-			console.log("user",user);
-			if (user) {
-				return res.status(400).json({ msg: 'User already exists' });
-			}
+    try {
+      let user = await User.findOne({ email });
+      console.log('user', user);
+      if (user) {
+        return res.status(400).json({ msg: 'User already exists' });
+      }
 
-			user = new User({
-				firstName,lastName, email,phone, date,gender,password,confirm_password
-			});
+      user = new User({
+        firstName,
+        lastName,
+        email,
+        phone,
+        date,
+        gender,
+        password,
+        confirm_password,
+      });
 
-			const salt = await bcrypt.genSalt(10);
+      const salt = await bcrypt.genSalt(10);
 
-			user.password = await bcrypt.hash(password, salt);
+      user.password = await bcrypt.hash(password, salt);
 
-			await user.save();
+      await user.save();
 
-			const payload = {
-				user: {
-					id: user.id
-				}
-			};
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
 
-			jwt.sign(
-				payload,
-				config.get('jwtSecret'),
-				{ expiresIn: 360000 },
-				(err, token) => {
-					if (err) throw err;
-					res.json({ token });
-				}
-			);
-		} catch (err) {
-			console.error(err.message);
-			res.status(500).send('Server error');
-		}
-	}
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server error');
+    }
+  }
 );
 
+router.get('/', async (req, res, next) => {
+  try {
+    const result = await User.find();
+    res.send(result);
+  } catch (error) {
+    console.log('data not found', error);
+  }
+});
 module.exports = router;
--- contact_keeper_api/server.js
+++ contact_keeper_api/server.js
@@ -1,25 +1,27 @@
-require('dotenv').config()
+require('dotenv').config();
 const express = require('express');
 const connectDB = require('./config/db');
 const app = express();
-const cors = require('cors')
-console.log("process.env",process.env.MONGOURI)
+const cors = require('cors');
+
+console.log('process.env', process.env.MONGOURI);
 // Connect Database
 connectDB();
 app.use(
-    cors({
-      origin: "*",
-    })
-  );
+  cors({
+    origin: '*',
+  })
+);
 
 // Init Middleware
 app.use(express.json({ extended: false }));
 
 // Define Routes
 app.use('/api/users', require('./routes/users'));
+// app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
-
+// app.use('/api/userslist', require('./routes/alluser'));
 const PORT = process.env.PORT || 5000;
 // Add headers before the routes are defined
 
